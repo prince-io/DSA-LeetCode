372b47cce995cfc51ba67794466113b3d2c0d34f
Prince Saini <princesaini7871@gmail.com>
2026-07-22 16:42:38 +0530

    Time: 1 ms (60.8%), Space: 42.9 MB (12.95%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int scoreOfParentheses(String s) {
+        Stack<Object> stk = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') stk.push(c);
+
+            else {
+                if (stk.peek() instanceof Character && (Character) stk.peek() == '(') {
+                    stk.pop();
+                    stk.push(1);
+                }
+
+                else {
+                    int count = 0;
+                    while (!(stk.peek() instanceof Character && (Character) stk.peek() == '(')) {
+    count += (Integer) stk.pop();
+}
+                    stk.pop();
+                    stk.push(count * 2);
+                }
+            }
+        }
+
+        int ans = 0;
+
+        while (!stk.isEmpty()) ans += (Integer) stk.pop();
+
+
+        return ans;
+    }
+}
