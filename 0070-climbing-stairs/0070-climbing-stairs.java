ae45d303d6fb4b954b0b5dd3a030d574d201d8ea
Prince Saini <princesaini7871@gmail.com>
2026-07-25 21:32:53 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.5%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] arr = new int[n + 1];
+        Arrays.fill(arr, -1);
+        return solve(n, arr);
+    }
+
+    public int solve(int n, int[] arr) {
+        if (n == 0)
+            return 1;
+        if (n < 0)
+            return 0;
+
+        if (arr[n] != -1)
+            return arr[n];
+
+        arr[n] = solve(n - 1, arr) + solve(n - 2, arr);
+
+        return arr[n];
+    }
+}
