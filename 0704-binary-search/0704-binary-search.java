131049ff8b1d75b83b793c50d46c489c6517e509
Prince Saini <princesaini7871@gmail.com>
2026-07-26 10:03:03 +0530

    Time: 0 ms (100%), Space: 48.4 MB (31.49%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,14 +1,40 @@
+// ITERATIVE APPROACH
+
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int l = 0, r = nums.length-1, mid;
+
+//         while (l <= r) {
+//             mid = l + (r - l) / 2;
+//             if (nums[mid] == target) return mid;
+//             else if (nums[mid] < target) l = mid + 1;
+//             else r = mid - 1;
+//         }
+
+//         return -1;
+//     }
+// }
+
+
+
+// RECURSIVE APPROACH
+
 class Solution {
     public int search(int[] nums, int target) {
-        int l = 0, r = nums.length-1, mid;
+        return bin(nums, target, 0, nums.length - 1);
+    }
 
-        while (l <= r) {
-            mid = l + (r - l) / 2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] < target) l = mid + 1;
-            else r = mid - 1;
-        }
+    public static int bin(int[] nums, int t, int l, int r) {
+        if (l > r)
+            return -1;
 
-        return -1;
+        int mid = l + (r - l) / 2;
+
+        if (nums[mid] == t)
+            return mid;
+        else if (nums[mid] < t)
+            return bin(nums, t, mid + 1, r);
+        else
+            return bin(nums, t, l, mid - 1);
     }
 }
