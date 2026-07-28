dc620966f148892682480da470f86316c0d58704
Prince Saini <princesaini7871@gmail.com>
2026-07-28 22:05:40 +0530

    Time: 2 ms (49.63%), Space: 52.8 MB (45.47%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -9,24 +9,53 @@
  *     }
  * }
  */
- 
+
+// public class Solution {
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         HashSet<ListNode> set = new HashSet<>();
+
+//         ListNode curr = headA;
+//         while (curr != null) {
+//             set.add(curr);
+//             curr = curr.next;
+//         }
+
+//         curr = headB;
+//         while (curr != null) {
+//             if (set.contains(curr))
+//                 return curr;
+//             curr = curr.next;
+//         }
+
+//         return curr;
+//     }
+// }
+
+
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashSet<ListNode> set = new HashSet<>();
+        ListNode a = headA, b = headB;
 
-        ListNode curr = headA;
-        while (curr != null) {
-            set.add(curr);
-            curr = curr.next;
+        boolean alist = true, blist = true;
+
+        while (a != b) {
+            a = a.next;
+            if (a == null) {
+                if (alist) a = headB;
+                else a = headA;
+                alist = !alist;
+            }
+
+            b = b.next;
+            if (b == null) {
+                if (blist) b = headA;
+                else b = headB;
+                blist = !blist;
+            }
+
+            if (a == headA && b == headB) return null;
         }
 
-        curr = headB;
-        while (curr != null) {
-            if (set.contains(curr))
-                return curr;
-            curr = curr.next;
-        }
-
-        return curr;
+        return a;
     }
 }
