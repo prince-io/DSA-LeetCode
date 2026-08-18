46cdadb3c6420e1d7218e7566999f312176289a6
Prince Saini <princesaini7871@gmail.com>
2026-08-18 18:20:40 +0530

    Time: 0 ms (100%), Space: 46.3 MB (55.26%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -10,88 +10,42 @@
  */
 
 class Solution {
-    // public ListNode reverseKGroup(ListNode head, int k) {
-    //     int c = 1;
-
-    //     ArrayList<ListNode> arr = new ArrayList<>();
-
-    //     ListNode curr = head;
-    //     ListNode start = head;
-
-    //     while(curr != null) {
-    //         curr = curr.next;
-            
-    //         if (curr != null) {
-    //             c++;
-    //             if (c % k == 0) {
-    //                 ListNode temp = curr.next;
-    //                 curr.next = null;
-
-    //                 arr.add(rev(start));
-
-    //                 start = temp;
-    //             }
-    //         }
-
-    //         else arr.add(start);
-    //     }
-
-    //     ListNode x = arr.get(0);
-
-    //     while (x.next != null) x = x.next;
-
-    //     for (int i = 1; i < arr.size(); i++) {
-    //         x.next = arr.get(i);
-    //         while (x.next != null) x = x.next;
-    //     }
-
-    //     return arr.get(0);
-    // }
-
-
-
     public ListNode reverseKGroup(ListNode head, int k) {
         int c = 0;
-
-        ArrayList<ListNode> arr = new ArrayList<>();
 
         ListNode curr = head;
         ListNode start = head;
 
-        while(curr != null) {
-            if (curr != null) c++;
+        ListNode last = null;
+        ListNode ans = null;
+
+        while (curr != null) {
+            c++;
 
             if (c == k) {
                 c = 0;
+
                 ListNode temp = curr.next;
                 curr.next = null;
 
-                arr.add(rev(start));
+                ListNode val = rev(start);
 
+                if (last != null)
+                    last.next = val;
+                else
+                    ans = val;
+
+                last = start;
                 start = temp;
                 curr = temp;
             }
 
-
-            else curr = curr.next;
-
-            // if (curr == null) arr.add(start);
+            else
+                curr = curr.next;
         }
 
-        arr.add(start);
-
-        
-
-        ListNode x = arr.get(0);
-
-        while (x.next != null) x = x.next;
-
-        for (int i = 1; i < arr.size(); i++) {
-            x.next = arr.get(i);
-            while (x.next != null) x = x.next;
-        }
-
-        return arr.get(0);
+        last.next = start;
+        return ans;
     }
 
     public static ListNode rev(ListNode head) {
