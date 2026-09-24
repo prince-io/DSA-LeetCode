1378a25d05851d21bb31cc3d98a78e03b9ce6e25
Prince Saini <princesaini7871@gmail.com>
2026-09-24 23:12:27 +0530

    Time: 73 ms (18.52%), Space: 148.2 MB (5.08%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -8,51 +8,27 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode removeNodes(ListNode head) {
         Stack<Integer> stk = new Stack<>();
         ListNode curr = head;
 
         while (curr != null) {
+            while (!stk.isEmpty() && stk.peek() < curr.val)
+                stk.pop();
             stk.push(curr.val);
             curr = curr.next;
-        }
-
-        int n = stk.size();
-        int[] arr = new int[n];
-        int[] mark = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = stk.get(i);
-        }
-        Arrays.fill(mark, -1);
-
-        stk.clear();
-        int x = n - 1;
-
-        while (x >= 0) {
-            if (!stk.isEmpty() && stk.peek() <= arr[x]) {
-                stk.pop();
-            } else if (stk.isEmpty()) {
-                stk.push(arr[x]);
-                mark[x] = 0;
-                x--;
-            } else {
-                stk.push(arr[x]);
-                x--;
-            }
         }
 
         curr = new ListNode();
         ListNode ans = curr;
 
-        for (int i = 0; i < n; i++) {
-            if (mark[i] == 0) {
-                curr.val = arr[i];
-                if (i < n - 1) {
-                    curr.next = new ListNode();
-                    curr = curr.next;
-                }
+        for (int i = 0; i < stk.size(); i++) {
+            curr.val = stk.get(i);
+            if (i < stk.size() - 1) {
+                curr.next = new ListNode();
+                curr = curr.next;
             }
         }
 
